c05a03ceedf3e205100f9b066069636cf9646004
jishnupunathil <jishnupunathil000@gmail.com>
2022-07-10 22:03:01 +0530

    home page created

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'books',component:BooksComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
